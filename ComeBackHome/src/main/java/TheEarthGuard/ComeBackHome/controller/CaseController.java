435b57853908ec8c8a7e52ebefe3be0069e596f4
HyeM207 <63229014+HyeM207@users.noreply.github.com>
2022-07-29 23:18:32 +0900

    Feat: Add Session to CaseForm
    사건 등록 페이지에서 폼 작성 후, 위치 설정하는 페이지 이동했다가 다시 돌아와도 데이터 유지되도록 함

--- ComeBackHome/src/main/java/TheEarthGuard/ComeBackHome/controller/CaseController.java
+++ ComeBackHome/src/main/java/TheEarthGuard/ComeBackHome/controller/CaseController.java
@@ -6,17 +6,23 @@ import TheEarthGuard.ComeBackHome.dto.CaseFormDto;
 import TheEarthGuard.ComeBackHome.dto.PlaceInfoDto;
 import TheEarthGuard.ComeBackHome.dto.SearchFormDto;
 import TheEarthGuard.ComeBackHome.service.CaseService;
+import TheEarthGuard.ComeBackHome.service.UserService;
 import java.sql.Timestamp;
 import java.util.List;
 import java.util.Optional;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
+@SessionAttributes({"caseForm"})
 public class CaseController {
     private final CaseService caseService;
     private UserService userService;
@@ -27,27 +33,46 @@ public class CaseController {
         this.userService = userService;
     }
 
-
     @GetMapping(value = "/cases/new")
-    public String createCaseForm() {
+    public String createCaseForm(Model model) {
+        model.addAttribute("caseForm", new CaseFormDto());
         return "cases/createCaseForm";
     }
 
     @PostMapping(value = "/cases/new")
-    public String updateCaseForm(@ModelAttribute PlaceInfoDto placeInfoDto, Model model) {
+    public String updateCaseForm(@ModelAttribute PlaceInfoDto placeInfoDto, @ModelAttribute("caseForm") CaseFormDto caseForm,HttpServletRequest request, Model model) {
         System.out.println(placeInfoDto.getMissing_area());
         System.out.println(placeInfoDto.getMissing_lat());
-        model.addAttribute("place", placeInfoDto);
+
+        // 위의 @ModelAttribute("caseForm"), SessionAttributes  코드로 자동으로 세션으로 객체를 저장해줌
+
+        // 세션 가져와서 placeInfoDto 정보 추가 후, model과 session에 저장
+//        HttpSession session = request.getSession();
+//        CaseFormDto form = (CaseFormDto) session.getAttribute("caseForm");
+        caseForm.setMissing_area(placeInfoDto.getMissing_area());
+        caseForm.setMissing_lat(placeInfoDto.getMissing_lat());
+        caseForm.setMissing_lng(placeInfoDto.getMissing_lng());
+
+        System.out.println("getMissing_lat : " + caseForm.getMissing_lat());
+        System.out.println("name : " + caseForm.getMissing_name());
+        System.out.println("sex : " + caseForm.getMissing_sex());
+//        session.setAttribute("caseForm", caseForm);
+        model.addAttribute("caseForm", caseForm);
         return "cases/createCaseForm";
     }
 
     @PostMapping(value = "/cases/new/submit")
-    public String uploadCaseForm(@ModelAttribute CaseFormDto form) {
+    public String uploadCaseForm(@Valid @ModelAttribute CaseFormDto form, Errors errors) {
+        if (errors.hasErrors()) {
+            System.out.println("ERROR!!!!!!!!");
+            return "/";
+        }
         User user = userService.findByEmail("hmk9667@gmail.com");
         System.out.println(user.getEmail());
 
         System.out.println("name : " + form.getMissing_name());
         System.out.println("missing_pic : " + form.getMissing_pic());
+        System.out.println("missing_sex : " + form.getMissing_sex());
         
         Case caseObj = Case.builder()
             .user(user)
@@ -76,24 +101,17 @@ public class CaseController {
     }
 
 
-//    @RequestMapping(value="/cases/new/missing-place/{option}", method= RequestMethod.GET)
-//    public String setMissingPlace(@RequestParam(value="option", required =false, defaultValue="1") String opt) {
-//        if (opt.equals("1")) {
-//            return "/cases/selectPlace";
-//        } else if (opt.equals("2")) {
-//            return "/cases/searchPlace";
-//        } else {
-//            return "/error/";
-//        }
-
-
-
-    @GetMapping(value = "/cases/new/selectPlace")
-    public String selectPlace() {
+    @PostMapping(value = "/cases/new/selectPlace")
+    public String selectPlace(@Valid @ModelAttribute CaseFormDto form, Model model, Errors errors) {
+        if (errors.hasErrors()) {
+            System.out.println("ERROR!!!!!!!!");
+            return "/";
+        }
+        model.addAttribute("caseForm", form); // 세션으로 같이 등록됨
         return "/cases/selectPlace";
     }
 
-    @GetMapping(value = "/cases/new/searchPlace")
+    @PostMapping(value = "/cases/new/searchPlace")
     public String searchPlace() {
         return "/cases/searchPlace";
     }
--- ComeBackHome/src/main/java/TheEarthGuard/ComeBackHome/dto/CaseFormDto.java
+++ ComeBackHome/src/main/java/TheEarthGuard/ComeBackHome/dto/CaseFormDto.java
@@ -8,6 +8,16 @@ import org.springframework.format.annotation.DateTimeFormat;
 @Getter
 @Setter
 public class CaseFormDto {
+//    private String missing_name;
+//    private String missing_pic;
+//    private String missing_age;
+//    private String missing_sex;
+//    private String missing_desc;
+//    private String missing_area;
+//    private String missing_lng;
+//    private String missing_lat;
+//    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+//    private LocalDateTime missing_time;
     private String missing_name;
     private String missing_pic;
     private Integer missing_age;
@@ -18,14 +28,4 @@ public class CaseFormDto {
     private String missing_lat;
     @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     private LocalDateTime missing_time;
-//    private String missing_name;
-//    private String missing_pic;
-//    private Integer missing_age;
-//    private Boolean missing_sex;
-//    private String missing_desc;
-//    private String missing_area;
-//    private String missing_lng;
-//    private String missing_lat;
-//    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
-//    private LocalDateTime missing_time;
 }
