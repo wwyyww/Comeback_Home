2ca4d4de8050d0b27695adbca5ff58891fe2a0ea
Chaeyeon99 <a01094779405@gmail.com>
2022-08-03 23:22:43 +0900

    fix: CaseResponseDto update

--- ComeBackHome/src/main/java/TheEarthGuard/ComeBackHome/repository/JpaReportRepository.java
+++ ComeBackHome/src/main/java/TheEarthGuard/ComeBackHome/repository/JpaReportRepository.java
@@ -1,51 +1,51 @@
-package TheEarthGuard.ComeBackHome.repository;
-
-import TheEarthGuard.ComeBackHome.domain.Report;
-import java.util.List;
-import java.util.Optional;
-import javax.persistence.EntityManager;
-
-public class JpaReportRepository implements ReportRepository{
-
-    private final EntityManager em;
-
-    public JpaReportRepository(EntityManager em){
-
-        this.em = em;
-    }
-
-    @Override
-    public Report save(Report reportObj) {
-        em.persist(reportObj);
-        return reportObj;
-    }
-
-    @Override
-    public Optional<Report> findByReportId(Long report_id) {
-        Report report = em.find(Report.class, report_id);
-        return Optional.ofNullable(report);
-    }
-
-    @Override
-
-    public Optional<Report> findByCaseId(String case_id) {
-        List<Report> result = em.createQuery("select r from Report r where r.cases.case_id = :case_id", Report.class)
-            .setParameter("case_id", case_id)
-            .getResultList();
-        return result.stream().findAny();
-    }
-
-    @Override
-    public Optional<Report> findByUserId(String user_id) {
-        List<Report> result = em.createQuery("select r from Report r where r.user.id = :user_id", Report.class)
-            .setParameter("user_id", user_id)
-            .getResultList();
-        return result.stream().findAny();
-    }
-
-    @Override
-    public List<Report> findAll() {
-        return em.createQuery("select r from Report r", Report.class)
-            .getResultList();
-    }
-}
+//package TheEarthGuard.ComeBackHome.repository;
+//
+//import TheEarthGuard.ComeBackHome.domain.Report;
+//import java.util.List;
+//import java.util.Optional;
+//import javax.persistence.EntityManager;
+//
+//public class JpaReportRepository implements ReportRepository{
+//
+////    private final EntityManager em;
+////
+////    public JpaReportRepository(EntityManager em){
+////
+////        this.em = em;
+////    }
+////
+////    @Override
+////    public Report save(Report reportObj) {
+////        em.persist(reportObj);
+////        return reportObj;
+////    }
+////
+////    @Override
+////    public Optional<Report> findByReportId(Long report_id) {
+////        Report report = em.find(Report.class, report_id);
+////        return Optional.ofNullable(report);
+////    }
+////
+////    @Override
+////
+////    public Optional<Report> findByCaseId(String case_id) {
+////        List<Report> result = em.createQuery("select r from Report r where r.cases.case_id = :case_id", Report.class)
+////            .setParameter("case_id", case_id)
+////            .getResultList();
+////        return result.stream().findAny();
+////    }
+////
+////    @Override
+////    public Optional<Report> findByUserId(String user_id) {
+////        List<Report> result = em.createQuery("select r from Report r where r.user.id = :user_id", Report.class)
+////            .setParameter("user_id", user_id)
+////            .getResultList();
+////        return result.stream().findAny();
+////    }
+////
+////    @Override
+////    public List<Report> findAll() {
+////        return em.createQuery("select r from Report r", Report.class)
+////            .getResultList();
+////    }
+//}
--- ComeBackHome/src/main/java/TheEarthGuard/ComeBackHome/repository/ReportRepository.java
+++ ComeBackHome/src/main/java/TheEarthGuard/ComeBackHome/repository/ReportRepository.java
@@ -5,10 +5,12 @@ import TheEarthGuard.ComeBackHome.dto.ReportRequestDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface ReportRepository extends JpaRepository<Report, Long> {
 
     @Query("select r from Report r where r.cases.case_id = :case_id")
